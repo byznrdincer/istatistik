060e6cbba5661aaa26154c4c8234fd4e5b2d4ab6
Beyzanur <beyzanurdincer502@gmail.com>
2025-04-10 11:46:47 +0300

    kac tane random sayı olacagı soruldu

--- BasitRastgeleOrnekleme.java
+++ BasitRastgeleOrnekleme.java
@@ -1,13 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class BasitRastgeleOrnekleme {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.println("please enter the  max number ");
-        int max=input.nextInt();
-        System.out.println("please enter the min number ");
-        int min=input.nextInt();
-        int random= (int)(Math.random()*(max-min+1))+min;
-        System.out.println("the random number is "+random);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the max number:");
+        int max = input.nextInt();
+
+        System.out.println("Please enter the min number:");
+        int min = input.nextInt();
+
+        System.out.println("How many random numbers do you want?");
+        int count = input.nextInt();
+
+        int totalNumbers = max - min + 1;
+
+        if (count > totalNumbers) {
+            System.out.println("You cannot request more unique numbers than the total range allows.");
+            return;
+        }
+
+        // Tüm olası sayıları listeye ekle
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            numbers.add(i);
+        }
+
+        // Listeyi karıştır
+        Collections.shuffle(numbers);
+
+        // İstenen kadar sayıyı yazdır
+        System.out.println("random numbers:");
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + ". random number: " + numbers.get(i));
+        }
     }
 }
